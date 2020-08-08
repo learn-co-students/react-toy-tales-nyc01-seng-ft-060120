4f11326c406e7b4f65db155a2ce07ff256708034
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-08-08 16:57:35 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/Header'
+import ToyHeader from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
-import data from './data'
+//import toyData from './data'
+
+let baseUrl = `http://localhost:3000/toys`
 
 
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toyData: []
   }
 
   handleClick = () => {
@@ -21,20 +24,76 @@ class App extends React.Component{
     })
   }
 
+  likeHandler = (obj, likes) => {
+    fetch(`${baseUrl}/${obj.id}`, {
+      method: "PATCH",
+      headers: {
+        "accepts": "application/json",
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+        likes: likes
+      })
+    })
+    .then(response => response.json())
+  }
+
+  // submitHandler = (obj) => {
+  //   let toysArray = [...this.state.toyData, obj]
+  //   this.setState({
+  //     toyData: toysArray
+  //   })
+  // }
+
+  submitHandler = (obj) => {
+    fetch(baseUrl, {
+      method: "POST",
+      headers: {
+        "accepts": "application/json",
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+        name: obj.name,
+        image: obj.image,
+        likes: obj.likes
+      })
+    })
+    .then(response => response.json())
+    .then(obj => {
+      let toysArray = [...this.state.toyData, obj]
+      this.setState({
+        toyData: toysArray
+      })
+    })
+  }
+
+  deleteToy = (obj) => {
+    fetch(`${baseUrl}/${obj.id}`, {
+      method: "DELETE"
+    })
+    let newToys = this.state.toyData.filter(toy => toy.id !== obj.id)
+    this.setState({
+      toyData: newToys
+    })
+  }
+
+  componentDidMount() {
+    fetch(baseUrl)
+    .then(response => response.json())
+    .then(toyFetch => this.setState({
+      toyData: toyFetch
+    }))
+  }
+
   render(){
     return (
       <>
-        <Header/>
-        { this.state.display
-            ?
-          <ToyForm/>
-            :
-          null
-        }
+        <ToyHeader/>
+        { this.state.display ? <ToyForm submitHandler={this.submitHandler}/> : null }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toyData={this.state.toyData} likeHandler={this.likeHandler} deleteToy={this.deleteToy}/>
       </>
     );
   }
--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ToyCard from './ToyCard'
 
-const ToyContainer = () => {
+function ToyContainer(props) {
   return(
     <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+      {props.toyData.map(tD => <ToyCard key={tD.id} likeHandler={props.likeHandler} deleteToy={props.deleteToy} toy={tD}/>)}
     </div>
   );
 }
