27f5718a71297ae3780fe79906fc4d066e9068ce
dr-dolce14 <dolcemascop1@gmail.com>
2020-08-09 14:42:54 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
-import data from './data'
+// import data from './data'
 
 
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toys: []
   }
 
   handleClick = () => {
@@ -21,20 +22,95 @@ class App extends React.Component{
     })
   }
 
+  renderNewLikes = (toyObj) => {
+    console.log(toyObj)
+    let updatedToys = this.state.toys.map(toy => {
+      if (toy.id === toyObj.id) {
+        return {...toy, likes: toyObj.likes }
+      } else {
+        return toy
+      }
+    })
+    this.setState({
+      toys: updatedToys
+    })
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    // .then(toyInfo => console.log(toyInfo))
+    .then(toyInfo => {
+      this.setState({ toys: toyInfo })
+    })
+  }
+
+  submitHandler = (obj) => {
+    fetch("http://localhost:3000/toys", {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: obj.name,
+        image: obj.image,
+        likes: obj.likes
+      })
+    })
+    .then(resp => resp.json())
+    .then(obj => {
+      let newToys = [...this.state.toys, obj]
+      this.setState({ toys: newToys })
+    }) 
+  }
+
+  updateLikes = (id, likes) => {
+    console.log(likes)
+    fetch(`http://localhost:3000/toys/${id}`, {
+      method: "PATCH",
+      headers: {
+        'content-type': "application/json",
+        'accept': "application/json"
+      },
+      body: JSON.stringify({
+        likes: likes
+    })
+  })
+    .then(resp => resp.json())
+    .then(toyObj => this.renderNewLikes(toyObj))
+  }
+
+  donateToy = (obj) => {
+    fetch(`http://localhost:3000/toys/${obj.id}`, {
+      method: "DELETE"
+    })
+    let newToys = this.state.toys.filter(toy => toy.id !== obj.id)
+    this.setState({
+      toys: newToys
+    })
+  }
+
+  
+
   render(){
+
+    // console.log(this.state.toys)
+    
+
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm submitHandler={this.submitHandler}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={this.state.toys} updateLikes={this.updateLikes} donateToy={this.donateToy}/>
       </>
     );
   }
--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -2,14 +2,38 @@ import React, { Component } from 'react';
 
 class ToyCard extends Component {
 
+  state = {
+    name: this.props.toy.name,
+    image: this.props.toy.image,
+    likes: this.props.toy.likes
+  }
+  
+  likeClicker = (e) => {
+    e.preventDefault()
+  
+    this.setState(previousState => {
+      return {
+        likes: previousState.likes + 1
+      }
+    }, () => {this.props.updateLikes(this.props.toy.id, this.state.likes)})
+    
+  }
+
+  deleteHandler = (e) => {
+    e.preventDefault()
+    this.props.donateToy(this.props.toy)
+  }
+
   render() {
+
+    // console.log(this.props)
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
-        <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <h2>{this.props.toy.name}</h2>
+        <img src={this.props.toy.image} alt={this.props.toy.name} className="toy-avatar" />
+        <p>{this.props.toy.likes} Likes </p>
+        <button onClick={this.likeClicker} className="like-btn">Like {'<3'}</button>
+        <button className="del-btn" onClick={this.deleteHandler}>Donate to GoodWill</button>
       </div>
     );
   }
