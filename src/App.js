51e369646ceabb2ce69c09456b83e1eccf8f01ad
edensongjbs <edensongjbs@gmail.com>
2020-08-09 15:31:33 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import ToyContainer from './components/ToyContainer'
 
 import data from './data'
 
+const url = "http://localhost:3000/toys"
 
 class App extends React.Component{
 
+  
+
   state = {
-    display: false
+    display: false,
+    toys: []
   }
 
   handleClick = () => {
@@ -21,24 +25,75 @@ class App extends React.Component{
     })
   }
 
+  removeFromToys = (toyId) => {
+    const newToys = this.state.toys.filter(toy => toy.id!==toyId)
+    this.setState({toys: newToys})
+  }
+
+  deleteHandler = (toyId) => {
+    fetch(`${url}/${toyId}`, {method: "DELETE"})
+    .then (this.removeFromToys(toyId))
+  }
+
+  replaceWithUpdatedLikes = (newToy) => {
+    const newToys = [...this.state.toys]
+    const foundToyIndex = newToys.findIndex(toy => toy.id===newToy.id)
+    newToys[foundToyIndex] = newToy
+    this.setState({toys: newToys})
+  }
+
+
+  updateLikes = (newToy) => {
+    newToy.likes++
+    const configObj = {
+      headers: {"Content-Type":"application/json"},
+      method: "PATCH",
+      body: JSON.stringify(newToy)
+    }
+    fetch(`${url}/${newToy.id}`, configObj)
+    .then( res => res.json())
+    .then( json => this.replaceWithUpdatedLikes(json))
+  }
+
+  componentDidMount() {
+    fetch(url)
+    .then(res => res.json())
+    .then(json => this.setState({toys: json}))
+  }
+
+  submitHandler = (newToy) => {
+    const configObj = {
+      headers: {"Content-Type":"application/json"},
+      method: "POST",
+      body: JSON.stringify(newToy)
+    }
+    fetch(url, configObj)
+    .then(res => res.json())
+    .then(json => {
+      const newToyAr = [...this.state.toys, json]
+      this.setState({toys: newToyAr})
+    })
+  }
+
   render(){
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm submitHandler={this.submitHandler}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer likeAddHandler={this.updateLikes} deleteHandler={this.deleteHandler} toys={this.state.toys}/>
       </>
     );
   }
 
 }
 
+export {url}
 export default App;
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 
 class ToyForm extends Component {
+  refreshState = {name:"", image:"", likes:0}
+  state = {...this.refreshState}
+
+  changeHandler = (e) => {
+    const newState = { [e.target.name]: e.target.value }
+    this.setState(newState)
+  }
+
+  formSubmitHandler = (e) => {
+    e.preventDefault()
+    const newToy = {...this.state}
+    this.setState(this.refreshState)
+    this.props.submitHandler(newToy)
+  }
 
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form className="add-toy-form" onSubmit={this.formSubmitHandler}>
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input onChange={this.changeHandler} value={this.state.name} type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input onChange={this.changeHandler} value={this.state.image} type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
           <br/>
-          <input type="submit" name="submit" value="Create New Toy" className="submit"/>
+          <input onChange={this.changeHandler} type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
       </div>
     );
