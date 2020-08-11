897c7197e014207bc212559807925fc324113b9b
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-08-11 07:10:13 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
-import data from './data'
+const API = "http://localhost:3000/toys"
 
 
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toyArray: []
   }
 
   handleClick = () => {
@@ -21,20 +22,70 @@ class App extends React.Component{
     })
   }
 
+  fetchToys = () => {
+    fetch(API)
+    .then(response => response.json())
+    .then(data => this.setState({ toyArray:data }))
+  }
+
+  componentDidMount(){
+    this.fetchToys()
+  }
+
+  submitHandler = (obj) => {
+    fetch(API, {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json',
+        application: 'application/json'
+      },
+      body: JSON.stringify(obj)
+    })
+    .then(response => response.json())
+    .then(data => this.fetchToys())
+  }
+
+  deleteHandler = (id) => {
+    fetch(`${API}/${id}`,{
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json',
+        application: 'application/json'
+      },
+    })
+    .then(response => response.json())
+    .then(data => this.fetchToys())
+  }
+
+  likeHandler = (id, newLikes) => {
+    fetch(`${API}/${id}`,{
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        application: 'application/json'
+      },
+      body: JSON.stringify({ likes:newLikes })
+    })
+    .then(response => response.json())
+    .then(data => this.fetchToys())
+  }
+
+
+
   render(){
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm submitHandler={this.submitHandler} />
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toyArray={this.state.toyArray} deleteHandler={this.deleteHandler}  likeHandler={this.likeHandler} />
       </>
     );
   }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 
 class ToyForm extends Component {
 
+  state = {
+    name: "", 
+    image: "",
+    likes: 0
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault()
+    this.props.submitHandler(this.state)
+  }
+
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]:e.target.value })
+  }
+
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form onSubmit={this.submitHandler} className="add-toy-form">
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text" onChange={this.changeHandler} />
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text" onChange={this.changeHandler} />
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
