43a59e312a2ab3a6b0bc268a6f87f0577559ebb4
Bearinawolfpack <bearinawolfpack@gmail.com>
2020-08-10 21:26:12 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
-
 import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
-import data from './data'
-
-
-class App extends React.Component{
-
+export default class App extends React.Component{
   state = {
-    display: false
+    display: false,
+    stateArray: []
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/toys/")
+      .then(resp => resp.json())
+      .then(toys => this.setState({stateArray: toys}))
   }
 
   handleClick = () => {
@@ -21,24 +23,51 @@ class App extends React.Component{
     })
   }
 
+  submitHandler = (obj) => {
+    // console.log(obj)
+    let submitArray = [obj, ...this.state.stateArray]
+    this.setState({stateArray: submitArray})
+
+    fetch("http://localhost:3000/toys/", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        'name': `${obj.name}`,
+        'image': `${obj.image}`,
+      })
+    }) // fetch POST
+    .then(resp => resp.json())
+    .then(toy => console.log(toy))
+  } // .submitHandler
+
+  clickHandler = (id) => {
+    // console.log(id)
+    let clickArray = [...this.state.stateArray]
+    let donateToy = clickArray.filter(toy => toy.id !== id)
+    // console.log(donateToy)
+    this.setState({stateArray: donateToy})
+
+    fetch(`http://localhost:3000/toys/${id}`, {
+    method: "DELETE"
+    })
+  }
+    
+
   render(){
+    // console.log(this.state.stateArray)
     return (
       <>
         <Header/>
-        { this.state.display
-            ?
-          <ToyForm/>
-            :
-          null
-        }
+        { this.state.display ? <ToyForm submitHandler={this.submitHandler}/> : null }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer stateArray={this.state.stateArray} clickHandler={this.clickHandler}/>
       </>
     );
   }
 
 }
-
-export default App;
